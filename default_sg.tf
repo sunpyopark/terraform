0f80c73a9e4e89940fc3ad99a193aaaccdda4675
teichae <chaeshkr@gmail.com>
2019-09-17 17:51:30 +0900

    Create default_sg.tf

---
+++ default_sg.tf
@@ -0,0 +1,24 @@
+#기본 생성 방식
+resource "aws_security_group" "web" {
+  vpc_id = "${module.vpc.vpc_id}"     #생성할 위치의 VPC ID
+  name = "WEB"                        #그룹 이름
+  description = "Terraform WEB SG"    #설명
+  ingress {
+    from_port = 22                    #인바운드 시작 포트
+    to_port = 22                      #인바운드 끝나는 포트
+    protocol = "tcp"                  #사용할 프로토콜
+    cidr_blocks = ["0.0.0.0/0"]       #허용할 IP 범위
+}
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+ ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+}
